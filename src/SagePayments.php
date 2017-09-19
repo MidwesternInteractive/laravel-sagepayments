6320ec38102fe5da761f6a3ada44cf3af850cfab
Ryan Doss <ryan.doss@gmail.com>
2017-09-19 16:23:16 -0500

    added basic config

--- src/SagePayments.php
+++ src/SagePayments.php
@@ -2,10 +2,15 @@
 
 namespace MidwesternInteractive\LaravelSagePayments;
 
+use Illuminate\Config\Repository;
+
 class SagePayments
 {
-    public function __construct()
+    public function __construct(Repository $configExternal)
     {
-        //
+        $merchId = $configExternal->get('sagepayments.merchid');
+        $merchKey = $configExternal->get('sagepayments.merchkey');
+        $devId = $configExternal->get('sagepayments.devid');
+        $devKey = $configExternal->get('sagepayments.devkey');
     }
 }
